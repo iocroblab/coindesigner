25c601c099952a7e2315829b58d62e39638e4f9b
jespa <jespa>
2007-11-16 09:59:31 +0000

    More MField subtypes support

--- src/mfield_editor.cpp
+++ src/mfield_editor.cpp
@@ -2,8 +2,8 @@
 #include <mfield_editor.h>
 #include <QLabel>
 #include <QTableWidget>
-#include <QColordialog>
-#include <QMessagebox>
+#include <QColorDialog>
+#include <QMessageBox>
 #include <Inventor/fields/SoFields.h>
 
 
@@ -41,43 +41,139 @@ MFieldEditor::MFieldEditor(SoMField *field, QWidget *p, Qt::WindowFlags f) : QDi
 		hh << "R" << "G" << "B";
         Ui.table->setHorizontalHeaderLabels(hh);
 
-        SoMFColor* soMFColor=(SoMFColor *)field;
-
         //Rellenamos la tabla
         SbColor color;
+        const SbColor *values = ((SoMFColor *)field)->getValues(0);
         for (int i=0; i<numFilas; i++)
         {
-           color=(*soMFColor)[i];       
-           Ui.table->item(i,0)->setText(S.setNum(color[0],'g',2));   
-           Ui.table->item(i,1)->setText(S.setNum(color[1],'g',2));   
-           Ui.table->item(i,2)->setText(S.setNum(color[2],'g',2));   
+           color= values[i].getValue();
+           Ui.table->setItem(i,0, new QTableWidgetItem(S.setNum(color[0],'g',2)));   
+           Ui.table->setItem(i,1, new QTableWidgetItem(S.setNum(color[1],'g',2)));   
+           Ui.table->setItem(i,2, new QTableWidgetItem(S.setNum(color[2],'g',2)));   
+        }      
+    }
+
+    //Tratamiento de campos SoMFVec2f
+    else if (!strcmp(nombre_tipo, "MFVec2f"))
+    {
+        //Cabecera de las columnas
+        Ui.table->setColumnCount(2);
+		hh << "X" << "Y";
+        Ui.table->setHorizontalHeaderLabels(hh);
+
+        //Rellenamos la tabla
+        const SbVec2f *values = ((SoMFVec2f *)field)->getValues(0);
+		float x, y;
+        for (int i=0; i<numFilas; i++)
+        {
+           values[i].getValue(x,y);       
+           Ui.table->setItem(i,0, new QTableWidgetItem(S.setNum(x,'g',5)));   
+           Ui.table->setItem(i,1, new QTableWidgetItem(S.setNum(y,'g',5)));   
         }      
     }
 
     //Tratamiento de campos SoMFVec3f
     else if (!strcmp(nombre_tipo, "MFVec3f"))
     {
-        //Columnas para R, G, B
+        //Cabecera de las columnas
         Ui.table->setColumnCount(3);
 		hh << "X" << "Y" << "Z";
         Ui.table->setHorizontalHeaderLabels(hh);
 
-        SoMFVec3f* soMFVec3f=(SoMFVec3f *)field;
-
         //Rellenamos la tabla
-        const SbVec3f *values = soMFVec3f->getValues(0);
+        const SbVec3f *values = ((SoMFVec3f *)field)->getValues(0);
 		float x, y, z;
-        //for (int i=0; i<numFilas; i++)
-        //{
-        //   values[i].getValue(x,y,z);       
-        //   Ui.table->item(i,0)->setText(S.setNum(x,'g',6));   
-        //   Ui.table->item(i,1)->setText(S.setNum(y,'g',6)); 
-        //   Ui.table->item(i,2)->setText(S.setNum(z,'g',6));    
-        //}      
+        for (int i=0; i<numFilas; i++)
+        {
+           values[i].getValue(x,y,z);       
+           Ui.table->setItem(i,0, new QTableWidgetItem(S.setNum(x,'g',5)));   
+           Ui.table->setItem(i,1, new QTableWidgetItem(S.setNum(y,'g',5)));   
+           Ui.table->setItem(i,2, new QTableWidgetItem(S.setNum(z,'g',5)));   
 
+			/* Esto tambien funciona, pero talvez es menos eficiente
+			//Leemos el campo y lo partimos en valores
+			SbString valueString;
+			field->get1(i, valueString);
+			QStringList vv = QString(valueString.getString()).split(" ");
+
+			//Insertamos cada valor en la tabla
+			for(int j=0;j<vv.size(); j++)
+    			Ui.table->setItem(i,j, new QTableWidgetItem(vv[j]));   
+
+			*/
+        }
     }
 
+    //Tratamiento de campos SoMFVec4f
+    else if (!strcmp(nombre_tipo, "MFVec4f"))
+    {
+        //Cabecera de las columnas
+        Ui.table->setColumnCount(4);
+		hh << "X" << "Y" << "Z" << "T";
+        Ui.table->setHorizontalHeaderLabels(hh);
+
+        //Rellenamos la tabla
+        const SbVec4f *values = ((SoMFVec4f *)field)->getValues(0);
+		float x, y, z, t;
+        for (int i=0; i<numFilas; i++)
+        {
+           values[i].getValue(x,y,z,t);       
+           Ui.table->setItem(i,0, new QTableWidgetItem(S.setNum(x,'g',5)));   
+           Ui.table->setItem(i,1, new QTableWidgetItem(S.setNum(y,'g',5)));   
+           Ui.table->setItem(i,2, new QTableWidgetItem(S.setNum(z,'g',5)));   
+           Ui.table->setItem(i,3, new QTableWidgetItem(S.setNum(t,'g',5)));   
+		   //TODO ¿hay que liberar luego los items?
+        }      
+    }
+
+    //Tratamiento de campos SoMFFloat, SoMFUInt32, SoMFInt32
+    else if (!strcmp(nombre_tipo, "MFFloat") || !strcmp(nombre_tipo, "MFUInt32") 
+                                             || !strcmp(nombre_tipo, "MFInt32") )
+    {
+        //Hace falta una columna
+        Ui.table->setColumnCount(1);
+		hh << tr("Value");
+        Ui.table->setHorizontalHeaderLabels(hh);
+
+        //Rellenamos la tabla usando la funcion get1 de coin
+        SbString valueString;
+        for (int i=0; i<numFilas; i++)
+        {
+           field->get1(i, valueString);
+           Ui.table->setItem(i,0, new QTableWidgetItem(valueString.getString()));
+        }      
+    }
+
+    //Tratamiento de campos SoMFString
+    else if (!strcmp(nombre_tipo, "MFString"))
+    {
+        //Hace falta una columna
+        Ui.table->setColumnCount(1);
+		hh << tr("Value");
+        Ui.table->setHorizontalHeaderLabels(hh);
+
+        //Rellenamos la tabla usando la funcion get1 de coin
+        SbString valueString;
+        for (int i=0; i<numFilas; i++)
+        {
+           field->get1(i, valueString);
+
+		   //Eliminamos las comillas
+           valueString.deleteSubString(0,0);
+           int ln = valueString.getLength();
+           valueString.deleteSubString(ln-1,ln-1);
+
+           Ui.table->setItem(i,0, new QTableWidgetItem(valueString.getString()));
+        }      
+    }
+
+	else
+	{
+		qDebug("Falta soporte para tipo %s\n", nombre_tipo);
+	}
+
 }
+
 
 void MFieldEditor::on_buttonBox_clicked(QAbstractButton * button)
 {
