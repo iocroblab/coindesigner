81b6c0b2453ca984704152e1efe385872b39b5bd
jespa <jespa>
2009-10-22 15:13:05 +0000

    Header in some files

--- src/src_editor.cpp
+++ src/src_editor.cpp
@@ -1,5 +1,5 @@
 /*
-    This file is part of coindesigner.
+    This file is part of coindesigner. (http://coindesigner.sf.net)
 
     coindesigner is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
+
 
 #include "src_editor.h"
 #include <QMessageBox>
